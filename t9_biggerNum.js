ea7201611ca969575752690e5391515302fb96c6
Elen <elen.ghazaryan6@gmail.com>
2021-12-06 00:39:50 +0400

    feat: numbers, bigger than given num

---
+++ t9_biggerNum.js
@@ -0,0 +1,6 @@
+function my_f(arr, num) {
+  let filtered = arr.filter((val) => val > num);
+
+  return filtered.length !== 0 ? filtered : "Such values do not exist.";
+}
+console.log(my_f([1, 1, 2, -3, 0, 8, 4, 0], 9));
